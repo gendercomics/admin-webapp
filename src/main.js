ffbec56e938c057e926181c7ecdd1ec1a3e23a6d
Michael Litschauer <michael.litschauer@ebcont.com>
2019-09-05 13:38:30 +0200

    corrected redirect rule

--- src/main.js
+++ src/main.js
@@ -1,10 +1,72 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueLogger from "vuejs-logger";
 import router from "./router";
+import * as Keycloak from 'keycloak-js'
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+const options = {
+    isEnabled: true,
+    logLevel: Vue.config.productionTip ? "error" : "debug",
+    stringifyArguments: false,
+    showLogLevel: true,
+    showMethodName: true,
+    separator: "|",
+    showConsoleColors: true
+};
+Vue.use(VueLogger, options);
+
+let initOptions = {
+    url: "http://sso.localhost/auth",
+    realm: "gendercomics",
+    clientId: "gendercomics-admin-app",
+    onLoad: "login-required"
+};
+
+let keycloak = Keycloak(initOptions);
+
+keycloak
+    .init({onLoad: initOptions.onLoad})
+    .success(auth => {
+        if (!auth) {
+            window.location.reload();
+        } else {
+            Vue.$log.info("Authenticated");
+        }
+
+        new Vue({
+            router,
+            render: h => h(App)
+        }).$mount("#app");
+
+        localStorage.setItem("access-token", keycloak.token);
+        localStorage.setItem("refresh-token", keycloak.refreshToken);
+        localStorage.setItem("id-token", keycloak.idToken);
+
+        setTimeout(() => {
+            keycloak
+                .updateToken(70)
+                .success(refreshed => {
+                    if (refreshed) {
+                        Vue.$log.debug("Token refreshed" + refreshed);
+                    } else {
+                        Vue.$log.warn(
+                            "Token not refreshed, valid for " +
+                            Math.round(
+                                keycloak.tokenParsed.exp +
+                                keycloak.timeSkew -
+                                new Date().getTime() / 1000
+                            ) +
+                            " seconds"
+                        );
+                    }
+                })
+                .error(() => {
+                    Vue.$log.error("Failed to refresh token");
+                });
+        }, 6000);
+    })
+    .error(() => {
+        Vue.$log.error("Authentication failed");
+    });
