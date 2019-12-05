b97c5c20816004be9e930ca4c986ceb69abcd9d1
Michael Litschauer <michael.litschauer@ebcont.com>
2019-12-05 13:28:50 +0100

    initial version: page header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,22 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark" sticky="true">
-            <b-navbar-brand href="#">GenderComics</b-navbar-brand>
+        <b-navbar toggleable="lg" type="dark" variant="dark" sticky>
+            <b-navbar-brand to="/">GenderComics</b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="#">Comics</b-nav-item>
+                    <b-nav-item to="comics">Comics</b-nav-item>
                     <b-nav-item href="#" disabled>Creators</b-nav-item>
+                    <b-nav-item href="#" disabled>Publishers</b-nav-item>
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
                     <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search not implemented yet"></b-form-input>
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="not implemented yet" disabled></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit" disabled>Search</b-button>
                     </b-nav-form>
 
                     <b-nav-item-dropdown right>
@@ -23,8 +24,8 @@
                         <template v-slot:button-content>
                             <em>User</em>
                         </template>
-                        <b-dropdown-item href="#">profile</b-dropdown-item>
-                        <b-dropdown-item href="#">logout</b-dropdown-item>
+                        <b-dropdown-item href="#" disabled>profile</b-dropdown-item>
+                        <b-dropdown-item to="logout">logout</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
             </b-collapse>
