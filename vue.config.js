9acd639c5dd1b9c987b44a719f8ea0faaf8d5bd5
Michael Litschauer <michael.litschauer@ebcont.com>
2019-12-11 14:16:09 +0100

    cors proxy config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      "http://api.localhost/": {
-        target: "http://localhost:8001",
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/admin/'
-      : '/',
-  runtimeCompiler: true
+    devServer: {
+        proxy: "http://localhost:8001"
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/admin/'
+        : '/',
+    runtimeCompiler: true
 };
