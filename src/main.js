41cb183d8af019f4173e722a0193fc5e3339c51c
Michael Litschauer <michael.litschauer@ebcont.com>
2020-02-12 14:30:08 +0100

    token refresh (untested)

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import * as Keycloak from "keycloak-js";
-import VueLogger from 'vuejs-logger';
-import BootstrapVue from 'bootstrap-vue';
-import './styles/styles.scss'
-import moment from 'moment';
+import VueLogger from "vuejs-logger";
+import BootstrapVue from "bootstrap-vue";
+import "./styles/styles.scss";
+import moment from "moment";
 
 Vue.config.productionTip = false;
 
@@ -18,14 +18,48 @@ let initOptions = {
 
 let keycloak = Keycloak(initOptions);
 
-/** Auth token interceptors */
-const authInterceptor = config => {
-    const token = localStorage.getItem('access-token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-};
+/* Adding the auth interceptors */
+// Vue.prototype.$api.interceptors.request.use(authInterceptor);
+
+// Add a request interceptor
+axios.interceptors.request.use(function(config) {
+  const token = localStorage.getItem("access-token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, function(error) {
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function(response) {
+  return response;
+}, function(error) {
+  const originalRequest = error.config;
+
+  if (error.response.status === 401 && originalRequest.url === "http://localhos:81/auth/token") {
+    router.push("/login");
+    return Promise.reject(error);
+  }
+
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    const refreshToken = localStorage.getItem("refresh-token");
+    return axios.post("http://localhos:81/auth/token",
+      {
+        "refresh_token": refreshToken
+      })
+      .then(res => {
+        if (res.status === 201) {
+          localStorage.setItem("access-token", res.data);
+          axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("access-token");
+          return axios(originalRequest);
+        }
+      });
+  }
+  return Promise.reject(error);
+});
 
 Vue.use({
     install(Vue) {
@@ -37,52 +71,38 @@ Vue.use({
 
 Vue.prototype.moment = moment;
 
-/** Adding the request interceptors */
-Vue.prototype.$api.interceptors.request.use(authInterceptor);
-
-const options = {
-    isEnabled: true,
-    logLevel: Vue.config.productionTip ? 'error' : 'debug',
-    stringifyArguments: false,
-    showLogLevel: true,
-    showMethodName: true,
-    separator: '|',
-    showConsoleColors: true
-};
-
-Vue.use(VueLogger, options);
 Vue.use(BootstrapVue);
 
-keycloak.init({onLoad: initOptions.onLoad}).success((auth) => {
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
 
-    if (!auth) {
-        window.location.reload();
-    } else {
-        Vue.$log.info("Authenticated");
-    }
+  if (!auth) {
+    window.location.reload();
+  } else {
+    Vue.$log.info("Authenticated");
+  }
 
-    new Vue({
-        router,
-        render: h => h(App)
-    }).$mount("#app");
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount("#app");
 
-    localStorage.setItem("access-token", keycloak.token);
-    localStorage.setItem("refresh-token", keycloak.refreshToken);
-    localStorage.setItem("id-token", keycloak.idToken);
+  localStorage.setItem("access-token", keycloak.token);
+  localStorage.setItem("refresh-token", keycloak.refreshToken);
+  localStorage.setItem("id-token", keycloak.idToken);
 
-    setTimeout(() => {
-        keycloak.updateToken(70).success((refreshed) => {
-            if (refreshed) {
-                Vue.$log.debug('Token refreshed' + refreshed);
-            } else {
-                Vue.$log.warn('Token not refreshed, valid for '
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-            }
-        }).error(() => {
-            Vue.$log.error('Failed to refresh token');
-        });
-    }, 60000)
+  setTimeout(() => {
+    keycloak.updateToken(70).success((refreshed) => {
+      if (refreshed) {
+        Vue.$log.debug("Token refreshed" + refreshed);
+      } else {
+        Vue.$log.warn("Token not refreshed, valid for "
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + " seconds");
+      }
+    }).error(() => {
+      Vue.$log.error("Failed to refresh token");
+    });
+  }, 60000);
 
 }).error(() => {
-    Vue.$log.error("Authentication failed!");
+  Vue.$log.error("Authentication failed!");
 });
