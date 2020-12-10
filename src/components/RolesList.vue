fa0605484b15031901d4316084b0e885ffd072c8
michael <michael.litschauer@gmail.com>
2020-12-10 12:50:47 +0100

    roles refactoring

--- src/components/RolesList.vue
+++ src/components/RolesList.vue
@@ -1,5 +1,73 @@
 <template>
     <div>
+        <b-container fluid class="mt-4">
+            <b-row>
+                <b-col lg="6" class="my-1">
+                    <b-form-group
+                        label="Filter"
+                        label-cols-sm="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="filterInput"
+                        class="mb-0"
+                    >
+                        <b-input-group size="sm">
+                            <b-form-input
+                                v-model="filter"
+                                type="search"
+                                id="filterInput"
+                                placeholder="Type to Search"
+                            ></b-form-input>
+                            <b-input-group-append>
+                                <b-button
+                                    :disabled="!filter"
+                                    @click="filter = ''"
+                                    >Clear
+                                </b-button>
+                            </b-input-group-append>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+                <b-col lg="6" class="my-1">
+                    <b-button to="roles/new" variant="outline-primary"
+                        >new role</b-button
+                    >
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col sm="5" md="6" class="my-1">
+                    <b-form-group
+                        label="Per page"
+                        label-cols-sm="6"
+                        label-cols-md="4"
+                        label-cols-lg="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="perPageSelect"
+                        class="mb-0"
+                    >
+                        <b-form-select
+                            v-model="perPage"
+                            id="perPageSelect"
+                            size="sm"
+                            :options="pageOptions"
+                        ></b-form-select>
+                    </b-form-group>
+                </b-col>
+
+                <b-col sm="7" md="6" class="my-1">
+                    <b-pagination
+                        v-model="currentPage"
+                        :total-rows="totalRows"
+                        :per-page="perPage"
+                        align="fill"
+                        size="sm"
+                        class="my-0"
+                    ></b-pagination>
+                </b-col>
+            </b-row>
+        </b-container>
+
         <b-container fluid class="pl-4 pr-4">
             <b-row>
                 <b-table
@@ -12,11 +80,53 @@
                     bordered
                     :fields="fields"
                     :items="roles"
+                    :current-page="currentPage"
+                    :per-page="perPage"
+                    :filter="filter"
+                    :filterIncludedFields="filterOn"
+                    @filtered="onFiltered"
                 >
                     <template v-slot:cell(actions)="row">
-                        <b-button size="sm" @click="edit(row.item)" class="mr-1"
-                            >edit
+                        <!-- edit button -->
+                        <b-button
+                            variant="light"
+                            size="sm"
+                            @click="edit(row.item)"
+                            class="mr-1"
+                        >
+                            <font-awesome-icon
+                                icon="edit"
+                                v-b-tooltip
+                                title="edit"
+                            />
                         </b-button>
+
+                        <!-- delete button -->
+                        <b-button
+                            variant="light"
+                            size="sm"
+                            class="mr-1"
+                            @click="deleteRole(row.item)"
+                        >
+                            <font-awesome-icon
+                                icon="trash"
+                                v-b-tooltip
+                                title="delete"
+                            />
+                        </b-button>
+                    </template>
+
+                    <!-- state -->
+                    <template v-slot:cell(metaData.status)="row">
+                        <span v-if="row.item.metaData.status === 'DRAFT'"
+                            ><b-badge variant="secondary">draft</b-badge></span
+                        >
+                        <span v-if="row.item.metaData.status === 'REVIEW'"
+                            ><b-badge variant="warning">review</b-badge></span
+                        >
+                        <span v-if="row.item.metaData.status === 'FINAL'"
+                            ><b-badge variant="success">final</b-badge></span
+                        >
                     </template>
 
                     <template v-slot:cell(metaData.changedOn)="data">
@@ -58,6 +168,7 @@ export default {
         return {
             fields: [
                 { key: 'actions', label: 'actions' },
+                { key: 'metaData.status', label: 'status' },
                 { key: 'name', label: 'name' },
                 { key: 'description', label: 'description' },
                 { key: 'metaData.changedOn', label: 'created/modified' },
@@ -66,6 +177,12 @@ export default {
             roles: null,
             loading: true,
             errored: false,
+            filter: null,
+            filterOn: [],
+            totalRows: 1,
+            currentPage: 1,
+            perPage: 10,
+            pageOptions: [10, 20, 50],
         };
     },
     mounted() {
@@ -82,6 +199,15 @@ export default {
         edit(item) {
             console.log('edit item: ' + item.id);
             this.$router.push('/roles/' + item.id);
+        },
+        deleteRole(item) {
+            console.log('delete role: ' + item.name);
+            // TODO implement delete logic
+        },
+        onFiltered(filteredItems) {
+            // Trigger pagination to update the number of buttons/pages due to filtering
+            this.totalRows = filteredItems.length;
+            this.currentPage = 1;
         },
     },
 };
