875b31c273e7a3b024dc83d3edb890e6e6629b07
michael <michael.litschauer@gmail.com>
2020-12-10 11:03:40 +0100

    publisher refactoring

--- src/components/PublisherList.vue
+++ src/components/PublisherList.vue
@@ -1,5 +1,73 @@
 <template>
     <div>
+        <b-container fluid class="mt-4">
+            <b-row>
+                <b-col lg="6" class="my-1">
+                    <b-form-group
+                        label="Filter"
+                        label-cols-sm="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="filterInput"
+                        class="mb-0"
+                    >
+                        <b-input-group size="sm">
+                            <b-form-input
+                                v-model="filter"
+                                type="search"
+                                id="filterInput"
+                                placeholder="Type to Search"
+                            ></b-form-input>
+                            <b-input-group-append>
+                                <b-button
+                                    :disabled="!filter"
+                                    @click="filter = ''"
+                                    >Clear
+                                </b-button>
+                            </b-input-group-append>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+                <b-col lg="6" class="my-1">
+                    <b-button to="publishers/new" variant="outline-primary"
+                        >new publisher</b-button
+                    >
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col sm="5" md="6" class="my-1">
+                    <b-form-group
+                        label="Per page"
+                        label-cols-sm="6"
+                        label-cols-md="4"
+                        label-cols-lg="3"
+                        label-align-sm="right"
+                        label-size="sm"
+                        label-for="perPageSelect"
+                        class="mb-0"
+                    >
+                        <b-form-select
+                            v-model="perPage"
+                            id="perPageSelect"
+                            size="sm"
+                            :options="pageOptions"
+                        ></b-form-select>
+                    </b-form-group>
+                </b-col>
+
+                <b-col sm="7" md="6" class="my-1">
+                    <b-pagination
+                        v-model="currentPage"
+                        :total-rows="totalRows"
+                        :per-page="perPage"
+                        align="fill"
+                        size="sm"
+                        class="my-0"
+                    ></b-pagination>
+                </b-col>
+            </b-row>
+        </b-container>
+
         <b-container fluid class="pl-4 pr-4">
             <b-row>
                 <b-table
@@ -12,10 +80,39 @@
                     bordered
                     :fields="fields"
                     :items="publishers"
+                    :current-page="currentPage"
+                    :per-page="perPage"
+                    :filter="filter"
+                    :filterIncludedFields="filterOn"
+                    @filtered="onFiltered"
                 >
                     <template v-slot:cell(actions)="row">
-                        <b-button size="sm" @click="edit(row.item)" class="mr-1"
-                            >edit
+                        <!-- edit button -->
+                        <b-button
+                            variant="light"
+                            size="sm"
+                            @click="edit(row.item)"
+                            class="mr-1"
+                        >
+                            <font-awesome-icon
+                                icon="edit"
+                                v-b-tooltip
+                                title="edit"
+                            />
+                        </b-button>
+
+                        <!-- delete button -->
+                        <b-button
+                            variant="light"
+                            size="sm"
+                            class="mr-1"
+                            @click="deletePublisher(row.item)"
+                        >
+                            <font-awesome-icon
+                                icon="trash"
+                                v-b-tooltip
+                                title="delete"
+                            />
                         </b-button>
                     </template>
 
@@ -73,6 +170,12 @@ export default {
             publishers: null,
             loading: true,
             errored: false,
+            filter: null,
+            filterOn: [],
+            totalRows: 1,
+            currentPage: 1,
+            perPage: 10,
+            pageOptions: [10, 20, 50],
         };
     },
     mounted() {
@@ -89,6 +192,14 @@ export default {
         edit(item) {
             console.log('edit item: ' + item.id);
             this.$router.push('/publishers/' + item.id);
+        },
+        onFiltered(filteredItems) {
+            // Trigger pagination to update the number of buttons/pages due to filtering
+            this.totalRows = filteredItems.length;
+            this.currentPage = 1;
+        },
+        deletePublisher(item) {
+            console.log('delete publisher: ' + item.name);
         },
     },
 };
--- src/views/Publishers.vue
+++ src/views/Publishers.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="text-left">
         <Header />
-
-        <b-col lg="6" class="mt-3">
-            <b-button to="publishers/new" variant="outline-primary"
-                >new publisher</b-button
-            >
-        </b-col>
-
         <PublisherList />
     </div>
 </template>
