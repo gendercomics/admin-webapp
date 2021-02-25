907001b7728b218f6c5326df624c3e297b4d2a0d
michael <michael.litschauer@gmail.com>
2021-02-25 20:53:19 +0100

    searchable input styling fix

--- src/components/ComicCreator.vue
+++ src/components/ComicCreator.vue
@@ -15,12 +15,13 @@
 
                 <!-- name -->
                 <b-dropdown
+                    no-flip
                     variant="outline-secondary"
                     :text="fullName(this.localValue.name)"
                 >
                     <b-dropdown-form @submit.stop.prevent="() => {}">
                         <b-form-group
-                            style="min-width: available"
+                            style="min-width: available;"
                             class="mb-0"
                             :description="searchDesc"
                         >
@@ -39,16 +40,18 @@
                         </b-form-group>
                     </b-dropdown-form>
                     <b-dropdown-divider />
-                    <b-dropdown-item-button
-                        v-for="option in availableOptions"
-                        :key="option.id"
-                        @click="onOptionClick(option)"
-                    >
-                        {{ fullName(option) }}
-                    </b-dropdown-item-button>
-                    <b-dropdown-text v-if="availableOptions.length === 0">
-                        no creator available to select
-                    </b-dropdown-text>
+                    <div style="max-height: 20em; overflow-y: auto">
+                        <b-dropdown-item-button
+                            v-for="option in availableOptions"
+                            :key="option.id"
+                            @click="onOptionClick(option)"
+                        >
+                            {{ fullName(option) }}
+                        </b-dropdown-item-button>
+                        <b-dropdown-text v-if="availableOptions.length === 0">
+                            no creator available to select
+                        </b-dropdown-text>
+                    </div>
                 </b-dropdown>
 
                 <!-- remove button -->
@@ -156,7 +159,10 @@ export default {
             if (creatorName.name != null) {
                 return creatorName.name;
             }
-            return creatorName.firstName + ' ' + creatorName.lastName;
+            if (creatorName.firstName != null && creatorName.lastName != null) {
+                return creatorName.firstName + ' ' + creatorName.lastName;
+            }
+            return '-- please select --';
         },
         onOptionClick(option) {
             this.$log.debug(
