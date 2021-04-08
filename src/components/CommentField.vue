c570dab60a64cc2fa851913ad5e1ecf9f391975e
michael <michael.litschauer@gmail.com>
2021-04-08 17:10:07 +0200

    saving issue fixed, isDirty check incomplete

--- src/components/CommentField.vue
+++ src/components/CommentField.vue
@@ -4,24 +4,33 @@
             <b-card no-body>
                 <template #header>
                     <div class="float-left">
+                        <span
+                            ><font-awesome-icon :icon="['far', 'comment']"
+                        /></span>
                         <span> {{ headerText }} on </span>
-                        <span v-if="localValue.metaData.changedOn == null">{{
-                            moment(localValue.metaData.createdOn).format(
-                                'DD.MM.YYYY HH:mm'
-                            )
-                        }}</span>
-                        <span v-else>{{
-                            moment(localValue.metaData.changedOn).format(
-                                'DD.MM.YYYY HH:mm'
-                            )
-                        }}</span>
+                        <span v-if="localValue.metaData != null">
+                            <span
+                                v-if="localValue.metaData.changedOn == null"
+                                >{{
+                                    moment(
+                                        localValue.metaData.createdOn
+                                    ).format('DD.MM.YYYY HH:mm')
+                                }}</span
+                            >
+                            <span v-else>{{
+                                moment(localValue.metaData.changedOn).format(
+                                    'DD.MM.YYYY HH:mm'
+                                )
+                            }}</span>
+                        </span>
                     </div>
                     <div class="float-right">
+                        <!--
                         <b-button
                             class="mr-1"
                             disabled
                             variant="warning"
-                            v-if="!saved"
+                            v-if="dirty"
                         >
                             <font-awesome-icon icon="exclamation-triangle" />
                         </b-button>
@@ -33,6 +42,7 @@
                         >
                             <font-awesome-icon icon="check-circle" />
                         </b-button>
+                        -->
                         <b-button @click="removeComment">
                             <font-awesome-icon icon="times-circle" />
                         </b-button>
@@ -42,18 +52,20 @@
             </b-card>
         </div>
 
+        <!--
         <b-container fluid class="mt-4 ml-4 mr-4">
             <b-row class="mt-4 mr-4">
                 <b-col id="json-text">
                     <b-card header="localvalue">
                         <pre class="mt-0">{{ localValue }}</pre>
                     </b-card>
-                    <b-card header="text">
-                        <pre class="mt-0">{{ $data.text }}</pre>
+                    <b-card header="originalValue">
+                        <pre class="mt-0">{{ originalValue }}</pre>
                     </b-card>
                 </b-col>
             </b-row>
         </b-container>
+        -->
     </div>
 </template>
 
@@ -85,7 +97,7 @@ export default {
                 value: null,
                 metadata: null,
             },
-            saved: false,
+            originalValue: null,
         };
     },
     computed: {
@@ -103,7 +115,20 @@ export default {
             if (this.localValue.metaData != null) {
                 userName = this.localValue.metaData.createdBy;
             }
-            return 'comment by ' + userName + timestamp;
+            return ' by ' + userName + timestamp;
+        },
+        dirty: function() {
+            let changed =
+                this.localValue === null && this.localValue.id === null;
+            changed =
+                changed && this.originalValue.value != this.localValue.value;
+            this.$log.debug('dirty=', changed);
+            return changed;
+        },
+    },
+    watch: {
+        '$data.text': function(newValue) {
+            this.localValue = newValue;
         },
     },
     methods: {
@@ -111,22 +136,23 @@ export default {
             this.$log.debug('comment-value=' + val);
             this.$data.text.value = val;
             this.saveText();
-            this.$nextTick(function() {
-                this.localValue = this.$data.text;
-            });
-            // TODO set localvalue (emit change)
         }, 1000),
         removeComment() {
             this.$log.debug('remove comment');
-            // TODO delete comment in database
-            this.$emit('remove', this.localValue);
+            // delete comment in database - if already stored
+            if (this.localValue.id != null) {
+                this.deleteText(this.localValue.id);
+            }
+            this.$nextTick(() => {
+                this.$emit('remove', this.localValue);
+            });
         },
     },
     mounted() {
-        this.$data.text = this.localValue;
-        if (this.localValue.value != null && this.localValue.value.id != null) {
-            this.saved = true;
-        }
+        this.$nextTick(() => {
+            this.$data.text = this.localValue;
+            this.originalValue = this.localValue;
+        });
     },
 };
 </script>
